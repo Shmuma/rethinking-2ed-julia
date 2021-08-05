f2273a9ba4d002e37609882e510f7af8fff3b08a
Max Lapan <max.lapan@gmail.com>
2021-08-05 20:34:42 +0200

    Julia package

---
+++ StatRethink.jl/hdpi.jl
@@ -0,0 +1,21 @@
+"""
+# hpdi
+Compute high density region.
+$(SIGNATURES)
+Derived from `hpd` in MCMCChains.jl.
+By default alpha=0.11 for a 2-sided tail area of p < 0.055% and p > 0.945%.
+"""
+function hpdi(x::Vector{T}; alpha=0.11) where {T<:Real}
+    n = length(x)
+    m = max(1, ceil(Int, alpha * n))
+
+    y = sort(x)
+    a = y[1:m]
+    b = y[(n - m + 1):n]
+    _, i = findmin(b - a)
+
+    return [a[i], b[i]]
+end
+
+export
+  hpdi
